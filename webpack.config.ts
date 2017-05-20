7896dcd55350570063f036b086dd9c74648d0200
Tanguy Krotoff <tkrotoff@gmail.com>
2017-05-20 18:44:03 +0200

    Optimize webpack output with CommonsChunkPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,26 @@
 import * as path from 'path';
+import { optimize } from 'webpack';
 
 module.exports = {
   entry: {
-    // Examples
     'examples/MDN_Form_validation/App': './examples/MDN_Form_validation/App.tsx',
     'examples/moduscreate/App': './examples/moduscreate/App.jsx',
     'examples/NativeFormWidgets/App': './examples/NativeFormWidgets/App.tsx',
-    'examples/password/App': './examples/password/App.tsx'
+    'examples/password/App': './examples/password/App.tsx',
+
+    'react-form-with-constraints': ['./src/FormWithConstraints.tsx', './src/Bootstrap4Helpers.tsx'],
+
+    react: ['react', 'prop-types', 'react-dom']
   },
 
   output: {
     path: path.join(__dirname, 'build'),
     filename: '[name].js'
   },
+
+  plugins: [
+    new optimize.CommonsChunkPlugin({names: ['react-form-with-constraints', 'react']})
+  ],
 
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
