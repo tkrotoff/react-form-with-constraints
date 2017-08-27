35b31880ea1d52c0cfddc4b2363cad2022226f34
Tanguy Krotoff <tkrotoff@gmail.com>
2017-08-28 00:25:43 +0200

    Improve npm-package-test

---
+++ npm-package-test/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo '** Clean'
+(cd Babel && rm -rf build node_modules yarn.lock)
+(cd TypeScript && rm -rf build node_modules yarn.lock)
+
+echo '** Running (cd .. && yarn)'
+(cd .. && yarn)
+
+echo '** Running yarn'
+(cd Babel && yarn)
+(cd TypeScript && yarn)
+
+echo '** Running (cd .. && rm -rf node_modules)'
+(cd .. && rm -rf node_modules)
+
+echo '** Running webpack'
+(cd Babel && yarn run webpack -d --env.development)
+(cd TypeScript && yarn run webpack -d --env.development)
+
+echo '** Clean'
+(cd Babel && rm -rf build node_modules yarn.lock)
+(cd TypeScript && rm -rf build node_modules yarn.lock)
