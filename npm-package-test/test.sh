3c49cbbcf6e098aa285033fa54c02952a6d76007
Tanguy Krotoff <tkrotoff@gmail.com>
2017-09-04 05:40:49 +0200

    Do not clean npm-package-test so it can be manually tested

--- npm-package-test/test.sh
+++ npm-package-test/test.sh
@@ -17,7 +17,3 @@ echo '** Running (cd .. && rm -rf node_modules)'
 echo '** Running webpack'
 (cd Babel && yarn run webpack -d --env.development)
 (cd TypeScript && yarn run webpack -d --env.development)
-
-echo '** Clean'
-(cd Babel && rm -rf build node_modules yarn.lock)
-(cd TypeScript && rm -rf build node_modules yarn.lock)
