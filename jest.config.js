aa2833028b3c98e719b248e82f72048cc5b95ca3
Tanguy Krotoff <tkrotoff@gmail.com>
2017-11-01 12:54:19 +0100

    Jest should ignore the examples directory

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
   },
+  testPathIgnorePatterns: ['/node_modules/', '/examples/'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 
