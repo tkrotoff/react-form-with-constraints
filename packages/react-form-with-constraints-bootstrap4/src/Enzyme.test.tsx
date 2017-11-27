c4ce7106be564a3dc890174c7b185447d4f56ad9
Tanguy Krotoff <tkrotoff@gmail.com>
2017-11-27 22:38:38 +0100

    Add API 'react-form-with-constraints-bootstrap4/lib/Enzyme'

--- packages/react-form-with-constraints-bootstrap4/src/Enzyme.test.tsx
+++ packages/react-form-with-constraints-bootstrap4/src/Enzyme.test.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-import { FieldFeedback, FieldFeedbacks } from 'react-form-with-constraints';
-
-// Cannot be '../../react-form-with-constraints/src/Enzyme' otherwise getFieldFeedbacksMessages() will fail
-import { getFieldFeedbacksMessages } from 'react-form-with-constraints/lib/Enzyme';
+import { FieldFeedback } from 'react-form-with-constraints';
 
 import * as Bootstrap4 from './index';
+import { getFieldFeedbacksMessages } from './Enzyme';
 
-test('with Bootstrap4', () => {
+test('getFieldFeedbacksMessages() with Bootstrap4', () => {
   const component = shallow(
     <div>
       <Bootstrap4.FormGroup for="username">
@@ -21,22 +19,6 @@ test('with Bootstrap4', () => {
   );
 
   const inputs = component.find(Bootstrap4.FormControlInput);
-  expect(inputs).toHaveLength(1);
-  expect(inputs.props().value).toEqual('');
-  expect(getFieldFeedbacksMessages(inputs, Bootstrap4.FieldFeedbacks)).toEqual(['Please fill out this field.']);
-});
-
-test.only('with matching FieldFeedbacks', () => {
-  const component = shallow(
-    <div>
-      <input name="username" value="" required />
-      <FieldFeedbacks for="username">
-        <FieldFeedback when="*" />
-      </FieldFeedbacks>
-    </div>
-  );
-
-  const inputs = component.find('input');
   expect(inputs).toHaveLength(1);
   expect(inputs.props().value).toEqual('');
   expect(getFieldFeedbacksMessages(inputs)).toEqual(['Please fill out this field.']);
---
+++ packages/react-form-with-constraints-bootstrap4/src/Enzyme.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { ShallowWrapper } from 'enzyme';
+
+import { getFieldFeedbacksMessages as _getFieldFeedbacksMessages } from 'react-form-with-constraints/lib/Enzyme';
+
+import { FieldFeedbacks } from './index';
+
+export function getFieldFeedbacksMessages(inputs: ShallowWrapper<React.InputHTMLAttributes<HTMLInputElement>, {}>) {
+  return _getFieldFeedbacksMessages(inputs, FieldFeedbacks);
+}
