f06754171c73d0a0c05b0eb4dd872ade44deca35
Tanguy Krotoff <tkrotoff@gmail.com>
2018-04-06 15:42:01 +0200

    Move unhandledRejection to JestSetup.ts

--- packages/react-form-with-constraints/src/FormWithConstraints.test.tsx
+++ packages/react-form-with-constraints/src/FormWithConstraints.test.tsx
@@ -12,12 +12,6 @@ function mount(node: React.ReactElement<FormWithConstraintsProps>) {
   return _mount<FormWithConstraintsProps, {}>(node);
 }
 
-// See Event: 'unhandledRejection' https://nodejs.org/api/process.html#process_event_unhandledrejection
-// See Bluebird Error management configuration http://bluebirdjs.com/docs/api/error-management-configuration.html
-process.on('unhandledRejection', (reason: Error | any, _promise: Promise<any>) => {
-  console.error('Unhandled promise rejection:', reason);
-});
-
 test('constructor()', () => {
   const form = new_FormWithConstraints({});
   expect(form.fieldsStore.fields).toEqual([]);
--- packages/react-form-with-constraints/src/JestSetup.ts
+++ packages/react-form-with-constraints/src/JestSetup.ts
@@ -6,3 +6,9 @@ configure({adapter: new Adapter()});
 // FIXME See console.assert not throwing with v22.4.0 https://github.com/facebook/jest/issues/5634
 import assert from 'assert';
 console.assert = assert;
+
+// See Event: 'unhandledRejection' https://nodejs.org/api/process.html#process_event_unhandledrejection
+// See Bluebird Error management configuration http://bluebirdjs.com/docs/api/error-management-configuration.html
+process.on('unhandledRejection', (reason: Error | any, _promise: Promise<any>) => {
+  console.error('Unhandled promise rejection:', reason);
+});
