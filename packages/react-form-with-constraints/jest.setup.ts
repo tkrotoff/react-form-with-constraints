cfc32e76c4f281b6b09db899995ec8b7a28d9d36
Tanguy Krotoff <tkrotoff@gmail.com>
2018-06-25 01:14:30 +0200

    Better document Node.js UnhandledPromiseRejectionWarning

--- packages/react-form-with-constraints/jest.setup.ts
+++ packages/react-form-with-constraints/jest.setup.ts
@@ -9,6 +9,14 @@ console.assert = assert;
 
 // See Event: 'unhandledRejection' https://nodejs.org/api/process.html#process_event_unhandledrejection
 // See Bluebird Error management configuration http://bluebirdjs.com/docs/api/error-management-configuration.html
+//
+// Node.js error:
+// (node:38141) UnhandledPromiseRejectionWarning: Unhandled promise rejection.
+// This error originated either by throwing inside of an async function without a catch block,
+// or by rejecting a promise which was not handled with .catch(). (rejection id: 4)
+// (node:38141) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated.
+// In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+//
 process.on('unhandledRejection', (reason: Error | any, _promise: Promise<any>) => {
   console.error('Unhandled promise rejection:', reason);
 });
