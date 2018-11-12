56cb2b2e2ae05f35e6650ce34a7d791206a122f6
Tanguy Krotoff <tkrotoff@gmail.com>
2018-11-13 00:56:35 +0100

    Remove mini-css-extract-plugin: not recognized by CodeSandbox?

--- examples/Bootstrap4/webpack.config.js
+++ examples/Bootstrap4/webpack.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -13,10 +12,6 @@ module.exports = {
     filename: '[name].js'
   },
 
-  plugins: [
-    new MiniCssExtractPlugin({filename: '[name].css'})
-  ],
-
   resolve: {
     extensions: ['.js', '.jsx']
   },
@@ -27,11 +22,9 @@ module.exports = {
       { test: /\.js$/, loader: 'source-map-loader' },
       { test: /\.html$/, loader: 'file-loader', options: {name: '[path][name].[ext]'} },
       {
-        // FIXME Don't know how to make source maps work
-        // See SourceMap not working with Webpack 4.8.1 https://github.com/webpack-contrib/mini-css-extract-plugin/issues/141
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          { loader: 'style-loader', options: {sourceMap: true} },
           { loader: 'css-loader', options: {sourceMap: true} },
           { loader: 'sass-loader', options: {sourceMap: true} }
         ]
