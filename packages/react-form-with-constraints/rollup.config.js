0369394e4716cac6a682c940b395ffef12534641
Tanguy Krotoff <tkrotoff@gmail.com>
2019-03-20 20:34:04 +0100

    No need to gzip *.js files

--- packages/react-form-with-constraints/rollup.config.js
+++ packages/react-form-with-constraints/rollup.config.js
@@ -2,16 +2,14 @@
 
 import typescript from 'rollup-plugin-typescript2';
 import { uglify } from 'rollup-plugin-uglify';
-import { gzip as zopfli } from 'node-zopfli';
-import gzip from 'rollup-plugin-gzip';
 import filesize from 'rollup-plugin-filesize';
 import strip from 'rollup-plugin-strip';
 
-const __PROD__ = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 function outputFileName() {
   let fileName = `react-form-with-constraints.${process.env.NODE_ENV}`;
-  fileName += __PROD__ ? '.min.js' : '.js';
+  fileName += isProd ? '.min.js' : '.js';
   return fileName;
 }
 
@@ -37,17 +35,11 @@ export default {
       tsconfigOverride: {compilerOptions: {module: 'esnext', inlineSources: false}}
     }),
 
-    __PROD__ && uglify(),
-
-    // With Zopfli: react-form-with-constraints.production.min.js.gz => 5 745 bytes
-    // With gzip: react-form-with-constraints.production.min.js.gz => 5 906 bytes
-    gzip({
-      customCompression: content => zopfli(Buffer.from(content))
-    }),
+    isProd && uglify(),
 
     filesize(),
 
-    __PROD__ && strip({
+    isProd && strip({
       include: ['**/*.js', '**/*.ts', '**/*.tsx'] // See https://github.com/rollup/rollup-plugin-strip/pull/7
     })
   ]
