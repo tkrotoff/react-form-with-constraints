fec748ed306ab2486412ca1ae830879525312ac2
Tanguy Krotoff <tkrotoff@gmail.com>
2019-06-15 19:05:04 +0200

    No need to specify Jest config moduleFileExtensions anymore

--- examples/Password/jest-e2e.config.js
+++ examples/Password/jest-e2e.config.js
@@ -1,5 +1,3 @@
-const { defaults } = require('jest-config');
-
 module.exports = {
   preset: 'jest-puppeteer',
 
@@ -9,6 +7,5 @@ module.exports = {
 
   // See [How to use with create-react-app](https://github.com/smooth-code/jest-puppeteer/issues/61)
   // See [Using Jest with Puppeteer and create-react-app](https://github.com/facebook/create-react-app/issues/4023)
-  testRegex: '\\.test\\.e2e\\.ts$',
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts']
+  testRegex: '\\.test\\.e2e\\.ts$'
 };
--- examples/ReactNative/jest.config.js
+++ examples/ReactNative/jest.config.js
@@ -1,14 +1,12 @@
 const { defaults } = require('jest-config');
 
-// FIXME Fix for error:
-// "React.createElement: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: object."
-// when writing "import App from './App'", Jest imports app.json instead of App.tsx
-const moduleFileExtensions = defaults.moduleFileExtensions.filter(
-  fileExtension => fileExtension !== 'json'
-);
-
 module.exports = {
   preset: 'jest-expo',
 
-  moduleFileExtensions: [...moduleFileExtensions, 'ts', 'tsx']
+  // FIXME Fix for error:
+  // "React.createElement: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: object."
+  // when writing "import App from './App'", Jest imports app.json instead of App.tsx
+  moduleFileExtensions: defaults.moduleFileExtensions.filter(
+    fileExtension => fileExtension !== 'json'
+  )
 };
