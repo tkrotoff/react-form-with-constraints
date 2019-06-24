26b999cec3fd99264d94e22c8ffec17113c48e51
Tanguy Krotoff <tkrotoff@gmail.com>
2019-06-25 01:24:27 +0200

    Silence ts-jest error TS151001

--- packages/react-form-with-constraints-material-ui/jest.config.js
+++ packages/react-form-with-constraints-material-ui/jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   setupFiles: ['../react-form-with-constraints/jest.setup.ts'],
-  preset: 'ts-jest'
+  preset: 'ts-jest',
+
+  globals: {
+    'ts-jest': {
+      // See https://github.com/kulshekhar/ts-jest/issues/748#issuecomment-423528659
+      //
+      // Ignore ts-jest error (false positive):
+      // "message TS151001: If you have issues related to imports, you should consider setting `esModuleInterop` to `true` in your TypeScript configuration file (usually `tsconfig.json`).
+      // See https://blogs.msdn.microsoft.com/typescript/2018/01/31/announcing-typescript-2-7/#easier-ecmascript-module-interoperability for more information."
+      diagnostics: {
+        ignoreCodes: [151001]
+      }
+    }
+  }
 };
