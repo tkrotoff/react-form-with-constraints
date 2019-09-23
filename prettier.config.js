0859b059be93f9c26496e6c7ef2fe9dcc065a163
Tanguy Krotoff <tkrotoff@gmail.com>
2019-09-23 15:26:37 +0200

    "[...] Your carpal tunnel will thank you for using single quotes"

--- prettier.config.js
+++ prettier.config.js
@@ -4,6 +4,9 @@ module.exports = {
   endOfLine: 'lf',
 
   // See https://github.com/airbnb/javascript/tree/eslint-config-airbnb-v16.1.0#strings--quotes
+  //
+  // ["Holding down the shift key every time you want a string kinda stinks. Your carpal tunnel will
+  // thank you for using single quotes."](https://github.com/airbnb/javascript/issues/269#issuecomment-107319162)
   singleQuote: true,
 
   // See https://github.com/airbnb/javascript/tree/eslint-config-airbnb-v16.1.0#whitespace--max-len
