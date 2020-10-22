fc529e9655a2a415ca89ba83a17d2dbaf308fe24
Tanguy Krotoff <tkrotoff@gmail.com>
2020-10-22 15:47:10 +0200

    Replace stylelint-config-airbnb with stylelint-config-twbs-bootstrap

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,24 +3,15 @@
 /** @type {Partial<import('stylelint').Configuration>} */
 const config = {
   extends: [
-    // As of 2020/03/18, stylelint-config-airbnb is still at v0.0.0
-    // Is this future proof?
-    // https://github.com/airbnb/css/pull/56
-    'stylelint-config-airbnb',
+    // /!\ Order matters: the next one overrides rules from the previous one
 
-    'stylelint-config-recommended-scss'
+    // Includes stylelint-config-recommended-scss and stylelint-config-standard
+    'stylelint-config-twbs-bootstrap/scss',
+
+    'stylelint-prettier/recommended'
   ],
 
-  rules: {
-    // Conflicts with Prettier
-    'string-quotes': null,
-    'number-leading-zero': null,
-
-    'rule-empty-line-before': null,
-    'at-rule-empty-line-before': null,
-    'scss/at-extend-no-missing-placeholder': null,
-    'order/order': null
-  }
+  rules: {}
 };
 
 module.exports = config;
